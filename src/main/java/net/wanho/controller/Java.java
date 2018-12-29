46c0f50811d7ae130974cfd94758208276f8fdf2
JIAHAO <2924063519@qq.com>
2018-12-29 16:36:34 +0800

    新增main方法

---
+++ src/main/java/net/wanho/controller/Java.java
@@ -0,0 +1,10 @@
+package net.wanho.controller;
+
+/**
+ * Created by yxmj on 2018/12/29.
+ */
+public class Java {
+    public static void main(String[] args) {
+        System.out.println("git与idea的整合2");
+    }
+}
